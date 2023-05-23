b33864ee065c476188147df76148746b84d26653
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-05-23 18:07:40 -0300

    Adicionado exercicio 04

---
+++ src/exercicio04/Program.java
@@ -0,0 +1,30 @@
+package exercicio04;
+
+import java.util.Scanner;
+
+//  Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e mostre-a expressa em dias.
+//  Leve em consideração o ano com 365 dias e o mês com 30. (Ex: 3 anos, 2 meses e 15 dias = 1170 dias.
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("-----------------------------------");
+        System.out.println("Boas-vindas ao Conversor de Idade em Dias!");
+        System.out.println("Você precisará informar a sua idade completa, ou seja, anos, meses e dias!");
+        System.out.println("Confuso? Segue um exemplo: 23 anos, 5 meses e 25 dias de Idade");
+        System.out.println("-----------------------------------");
+
+        System.out.println("Digite a sua quantidade de anos: ");
+        int anos = sc.nextInt();
+        System.out.println("Digite a sua quantidade de meses: ");
+        int meses = sc.nextInt();
+        System.out.println("Digite a sua quantidade de dias: ");
+        int dias = sc.nextInt();
+
+        int idadeDias = (anos * 365) + (meses * 30) + (dias);
+        System.out.println("-----------------------------------");
+        System.out.println("Sua idade em dias é: " + idadeDias);
+
+
+    }
+}
